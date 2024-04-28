893ee3e3da031cae9b8c39ca93491482fc997f1e
bytorbrynden <brynden.bielefeld@gmail.com>
2024-04-27 22:02:58 -0500

    [Rock Paper Scissors] Logic to handle computer and player choices
    Although not required, also added a simple UI to play the game.

--- foundations-rock-paper-scissors/rockpaperscissors.js
+++ foundations-rock-paper-scissors/rockpaperscissors.js
@@ -1,1 +1,45 @@
-console.log("Rock Paper Scissors script loaded!");
+const POSSIBLE_CHOICES = ["Rock", "Paper", "Scissors"];
+let developmentMode = true;
+
+let developmentMessage = (msg) => {
+    if (developmentMode) console.log("[DEV] " + msg);
+};
+
+let toggleDevMode = () => {
+    developmentMode = !developmentMode;
+};
+
+let playButtonClick = () => {
+    getComputerChoice();
+    getHumanChoice();
+};
+
+let getComputerChoice = () => {
+    let numericChoice = Math.floor(Math.random() * POSSIBLE_CHOICES.length);
+    let englishChoice = POSSIBLE_CHOICES[numericChoice];
+
+    developmentMessage("Computer's numeric choice: " + numericChoice);
+    developmentMessage("Computer's computed choice: " + englishChoice);
+
+    return numericChoice;
+};
+
+let getHumanChoice = () => {
+    let numericChoice = +prompt("Enter your choice!");
+
+    if (numericChoice >= POSSIBLE_CHOICES.length) {
+        developmentMessage("Player provided invalid choice, trying again.");
+        alert("Invalid selection, available choices are 0, 1, and 2.");
+
+        return getHumanChoice();
+    }
+
+    let englishChoice = POSSIBLE_CHOICES[numericChoice];
+
+    developmentMessage("Human's numeric choice: " + numericChoice);
+    developmentMessage("Human's computed choice: " + englishChoice);
+
+    return numericChoice;
+};
+
+developmentMessage("Rock Paper Scissors script loaded!");
