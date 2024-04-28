37165302b214e577609aa01ec96312b6c44ac204
bytorbrynden <brynden.bielefeld@gmail.com>
2024-04-27 22:30:55 -0500

    [Rock Paper Scissors] Created logic to play a round
    For now we will return a string that says who wins, will add scoring next.

--- foundations-rock-paper-scissors/rockpaperscissors.js
+++ foundations-rock-paper-scissors/rockpaperscissors.js
@@ -1,4 +1,10 @@
 const POSSIBLE_CHOICES = ["Rock", "Paper", "Scissors"];
+const GAMEPLAY_RULES   = {
+    "Rock": POSSIBLE_CHOICES[1],
+    "Paper": POSSIBLE_CHOICES[2],
+    "Scissors": POSSIBLE_CHOICES[0]
+};
+
 let developmentMode = true;
 
 let developmentMessage = (msg) => {
@@ -7,11 +13,6 @@ let developmentMessage = (msg) => {
 
 let toggleDevMode = () => {
     developmentMode = !developmentMode;
-};
-
-let playButtonClick = () => {
-    getComputerChoice();
-    getHumanChoice();
 };
 
 let getComputerChoice = () => {
@@ -27,9 +28,10 @@ let getComputerChoice = () => {
 let getHumanChoice = () => {
     let numericChoice = +prompt("Enter your choice!");
 
+    // Humans can make errors, so let's perform a simple check to make sure the value the user entered is valid.
     if (numericChoice >= POSSIBLE_CHOICES.length) {
         developmentMessage("Player provided invalid choice, trying again.");
-        alert("Invalid selection, available choices are 0, 1, and 2.");
+        alert("Invalid selection, available choices are 0, 1, or 2.");
 
         return getHumanChoice();
     }
@@ -40,6 +42,27 @@ let getHumanChoice = () => {
     developmentMessage("Human's computed choice: " + englishChoice);
 
     return numericChoice;
+};
+
+let playRound = (computerChoice, humanChoice) => {
+    let computerChoiceStr = POSSIBLE_CHOICES[computerChoice];
+    let humanChoiceStr    = POSSIBLE_CHOICES[humanChoice];
+    let beatsComputer     = GAMEPLAY_RULES[computerChoiceStr];
+    let beatsHuman        = GAMEPLAY_RULES[humanChoiceStr];
+
+    developmentMessage("Computer chose " + computerChoiceStr + ". " + beatsComputer + " would win.");
+    developmentMessage("Human chose " + humanChoiceStr + ". " + beatsHuman + " would win.");
+
+    if (computerChoice == humanChoice) {
+        return "It's a tie!";
+    } else {
+        if (computerChoiceStr == beatsHuman) return "Computer wins!";
+        else if (humanChoiceStr == beatsComputer) return "Human wins!";
+    }
+};
+
+let playButtonClick = () => {
+    alert(playRound(getComputerChoice(), getHumanChoice()));
 };
 
 developmentMessage("Rock Paper Scissors script loaded!");
